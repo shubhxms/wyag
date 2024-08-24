ed49cf52e300e67b4dc2080dfe4d10f97900ea06
Shubham Shah <03.shubhamshah@gmail.com>
2024-08-24 11:43:00 +0530

    basic structure + working on init

---
+++ libwyag.py
@@ -0,0 +1,70 @@
+import argparse, collections, configparser, grp, pwd, hashlib, os, re, sys, zlib
+from datetime import datetime
+from fnmatch import fnmatch
+from math import ceil
+
+
+argparser = argparse.ArgumentParser(description="Some Arguments")
+argsubparsers = argparser.add_subparsers(title="commands", dest="command", required=True)
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_checkout(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
+
+class GitRepository(object):
+
+    worktree = None
+    gitdir = None
+    conf = None
+
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not(force or os.path.isdir(self.gitdir)):
+            raise Exception("%s is not a Git repo" % path)
+        
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file is missing")
+        
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+            
+
+    def repo_path(repo, *path):
+        return os.path.join(repo.gitdir, *path)
+
+
+
+
+
+
+
+
+
+
+
+
