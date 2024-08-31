c3b1b35578aa94a42ac94abfca6e3d263fabba50
Shubham Shah <03.shubhamshah@gmail.com>
2024-08-31 16:03:09 +0530

    EO Ch. 5

--- libwyag.py
+++ libwyag.py
@@ -9,6 +9,7 @@ argsubparsers = argparser.add_subparsers(
     title="commands", dest="command", required=True
 )
 
+
 def main(argv=sys.argv[1:]):
     args = argparser.parse_args(argv)
     match args.command:
@@ -46,6 +47,124 @@ def main(argv=sys.argv[1:]):
             print("Bad command.")
 
 
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument(
+    "path",
+    metavar="directory",
+    nargs="?",
+    default=" . ",
+    help="Where to create the repository.",
+)
+
+
+def cmd_init(args):
+    repo_create(args.path)
+
+
+argsp = argsubparsers.add_parser(
+    "cat-file", help="Provide content of repository objects"
+)
+argsp.add_argument(
+    "type",
+    metavar="type",
+    choices=["blob", "commit", "tag", "tree"],
+    help="Specify the type",
+)
+argsp.add_argument("object", metavar="object", help="The object to display")
+
+
+def cmd_cat_file(args):
+    repo = repo_find()
+    cat_file(repo, args.object, fmt=args.type.encode())
+
+
+def cat_file(repo, obj, fmt=None):
+    obj = object_read(repo, object_find(repo, obj, fmt=fmt))
+
+
+def object_find(repo, name, fmt=None, follow=True):
+    return name
+
+
+argsp = argsubparsers.add_parser(
+    "hash-object", help="Compute object ID and optionally creates a blob from a file"
+)
+argsp.add_argument(
+    "-t",
+    metavar="type",
+    dest="type",
+    choices=["blob", "commit", "tag", "tree"],
+    default="blob",
+    help="Specify the type",
+)
+argsp.add_argument(
+    "-w",
+    dest="write",
+    action="store_true",
+    help="Actually write the object into the database",
+)
+argsp.add_argument("path", help="Read object from <file>")
+
+
+def cmd_hash_object(args):
+    repo = repo_find() if args.write else None
+    with open(args.path, "rb") as fd:
+        sha = object_hash(fd, args.type.encode(), repo)
+        print(sha)
+
+
+def object_hash(fd, fmt, repo=None):
+    data = fd.read()
+    match fmt:
+        case b"commit":
+            obj = GitCommit(data)
+        case b"tree":
+            obj = GitTree(data)
+        case b"tag":
+            obj = GitTag(data)
+        case b"blob":
+            obj = GitBlob(data)
+        case _:
+            raise Exception("Unknown type %s!" % fmt)
+    return object_write(obj, repo)
+
+
+argsp = argsubparsers.add_parser("log", help="Display history of a given commit.")
+argsp.add_argument("commit", default="HEAD", nargs="?", help="Commit to start at.")
+def cmd_log(args):
+    repo = repo_find()
+    print("digraph wyaglog{")
+    print("  node[shape=rect]")
+    log_graphviz(repo, object_find(repo, args.commit),  set())
+    print("}")
+
+def log_graphviz(repo, sha, seen):
+    if sha in seen:
+        return
+    seen.add(sha)
+    commit = object.read(repo, sha)
+    short_hash = sha[0:8]
+    message = commit.kvlm[None].decode("utf8").strip()
+    message = message.replace("\\", "\\\\")
+    message = message.replace("\"", "\\\"")
+
+    if "\n" in message:
+        message = message[:message.index("\n")]
+    
+    print("c_{0} [label=\"{1}: {2}\"]".format(sha, sha[0:7], message))
+    assert commit.fmt==b'commit'
+
+    if not b'parent' in commit.kvlm.keys(): return
+    parents = commit.kvlm[b'parent']
+
+    if type(parents) != list: parents = [ parents ]
+
+    for p in parents:
+        p.decode("ascii")
+        print("  c_{0} -> c_{1};".format(sha, p))
+        log_graphviz(repo, p, seen)
+
+
 class GitRepository(object):
 
     worktree = None
@@ -72,8 +191,10 @@ class GitRepository(object):
             if vers != 0:
                 raise Exception("Unsupported repositoryformatversion %s" % vers)
 
+
 def repo_path(repo, *path):
     return os.path.join(repo.gitdir, *path)
+
 
 def repo_dir(repo, *path, mkdir=False):
     path = repo_path(repo, *path)
@@ -89,6 +210,7 @@ def repo_dir(repo, *path, mkdir=False):
         return path
     else:
         return None
+
 
 def repo_file(repo, *path, mkdir=False):
     if repo_dir(repo, *path[:-1], mkdir=mkdir):
@@ -125,6 +247,7 @@ def repo_create(path):
 
     return repo
 
+
 def repo_default_config():
     ret = configparser.ConfigParser()
 
@@ -135,47 +258,158 @@ def repo_default_config():
 
     return ret
 
-argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
-argsp.add_argument(
-    "path",
-    metavar="directory",
-    nargs="?",
-    default=" . ",
-    help="Where to create the repository.",
-)
 
 def repo_find(path=".", required=True):
     path = os.path.realpath(path)
 
     if os.path.isdir(os.path.join(path, ".git")):
         return GitRepository(path)
-    
+
     parent = os.path.realpath(os.path.join(path, ".."))
 
     if parent == path:
         if required:
             raise Exception("No  Git Dir")
         return None
-    
+
     return repo_find(parent, required)
 
 
+class GitObject(object):
+
+    def __init__(self, data=None):
+        if data != None:
+            self.deserialize(data)
+        else:
+            self.init()
+
+    def serialize(self, repo):
+        raise Exception("unimplemented")
+
+    def deserialize(self, data):
+        raise Exception("unimplemented")
+
+    def init(self):
+        pass
 
 
+def object_read(repo, sha):
+    path = repo_file(repo, "objects", sha[0:2], sha[2:1])
 
-def cmd_init(args):
-    repo_create(args.path)
+    if not os.path.isfile(path):
+        return None
 
-    
+    with open(path, "rb") as f:
+        raw = zlib.decompress(f.read())
+
+        x = raw.find(b" ")
+        fmt = raw[0:x]
+
+        y = raw.find(b"\x00", x)
+        size = int(raw[x:y].decode("ascii"))
+        if size != len(raw) - y - 1:
+            raise Exception("Malformed object {0}: bad length".format(sha))
+
+        match fmt:
+            case b"commit":
+                c = GitCommit
+            case b"tree":
+                c = GitTree
+            case b"tag":
+                c = GitTag
+            case b"blob":
+                c = GitBlob
+            case _:
+                raise Exception(
+                    "Unknown type {0} for object {1}".format(fmt.decode("ascii"), sha)
+                )
+
+        return c(raw[y + 1 :])
 
 
+def object_write(obj, repo=None):
+    data = obj.serialize()
+    result = obj.fmt + b" " + str(len(data)).encode() + b"\x00" + data
+    sha = hashlib.sha1(result).hexdigest()
+
+    if repo:
+        path = repo_file(repo, "objects", sha[0:2], sha[2:], mkdir=True)
+
+        if os.path.exists(path):
+            with open(path, "wb") as f:
+                f.write(zlib.compress(result))
 
 
+class GitBlob(GitObject):
+    fmt = b"blob"
+
+    def serialize(self):
+        return self.blobdata
+
+    def deserialize(self, data):
+        self.blobdata = data
 
 
+def kvlm_parse(raw, start=0, dct=None):
+    if not dct:
+        dct = collections.OrderedDict()
+
+    spc = raw.find(b" ", start)
+    nl = raw.find(b"\n", start)
+
+    if (spc < 0) or (nl < spc):
+        assert nl == start
+        dct[None] = raw[start + 1 :]
+        return dct
+
+    key = raw[start:spc]
+
+    end = start
+    while True:
+        end = raw.find(b"\n", end + 1)
+        if raw[end + 1] != ord(" "):
+            break
+
+    value = raw[spc + 1 : end].replace(b"\n ", b"\n")
+
+    if key in dct:
+        if type(dct[key]) == list:
+            dct[key].append(value)
+        else:
+            dct[key] = [dct[key], value]
+    else:
+        dct[key] = value
+
+    return kvlm_parse(raw, start=end + 1, dct=dct)
 
 
+def kvlm_serialize(kvlm):
+    ret = b" "
+
+    for k in kvlm.keys():
+        if k == None:
+            continue
+        val = kvlm[k]
+
+        if type(val) != list:
+            val = [val]
+
+        for v in val:
+            ret += k + b" " + (v.replace(b"\n", b"\n ")) + b"\n"
+
+    ret += b"\n" + kvlm[None] + b"\n"
+
+    return ret
 
 
+class GitCommit(GitObject):
+    fmt = b"commit"
 
+    def deserialize(self, data):
+        self.kvlm = kvlm_parse(data)
 
+    def serialize(self):
+        return kvlm_serialize(self.kvlm)
+
+    def init(self):
+        self.kvlm = dict()
